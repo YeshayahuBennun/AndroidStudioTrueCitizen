26746eb4046db0e35dc67cd5eb2a34d46b80f576
YeshayahuBennun <ybennun@yahoo.com>
2022-01-04 21:18:18 +0200

    commit#5 - True Citizen APP -  Setup the Next Button and Show Questions

--- app/src/main/java/com/ybennun/truecitizen/MainActivity.java
+++ app/src/main/java/com/ybennun/truecitizen/MainActivity.java
@@ -4,23 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.ybennun.truecitizen.databinding.ActivityMainBinding;
 import com.ybennun.truecitizen.model.Question;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private int currentQuestionIndex = 0;
+
+
     private Question[] questionBank = new Question[]{
             //create/instantiate question objects
-            new Question(R.string.question_amendments,false),//correct: 27
-            new Question(R.string.question_constitution,true),
-            new Question(R.string.question_declaration,true),
-            new Question(R.string.question_independence_rights,true),
-            new Question(R.string.question_religion,true),
-            new Question(R.string.question_government,false),
-            new Question(R.string.question_government_feds,false),
-            new Question(R.string.question_government_senators,false),
-
+            new Question(R.string.question_amendments, false),//correct: 27
+            new Question(R.string.question_constitution, true),
+            new Question(R.string.question_declaration, true),
+            new Question(R.string.question_independence_rights, true),
+            new Question(R.string.question_religion, true),
+            new Question(R.string.question_government, false),
+            new Question(R.string.question_government_feds, false),
+            new Question(R.string.question_government_senators, false),
     };
 
     @Override
@@ -28,8 +32,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        binding.questionTextView.setText(questionBank[0].getAnswerResId());
+        binding.questionTextView.setText(questionBank[currentQuestionIndex].getAnswerResId());
+
+        binding.nextButton.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questionBank.length;//incrementing
+
+            updateQuestion();
+        });
+
+        binding.prevButton.setOnClickListener(view -> {
+            if (currentQuestionIndex > 0) {
+                currentQuestionIndex = (currentQuestionIndex - 1) % questionBank.length;//decrementing
+                updateQuestion();
+            }
+        });
+
+    }
+
+    private void updateQuestion() {
+        binding.questionTextView.setText(questionBank[currentQuestionIndex].getAnswerResId());
     }
 }
